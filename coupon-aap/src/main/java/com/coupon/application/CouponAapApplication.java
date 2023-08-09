2adf6d1fad19b29e0af9e23467f421da4364c15a
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2023-08-09 11:35:44 +0530

    Initial commit

---
+++ coupon-aap/src/main/java/com/coupon/application/CouponAapApplication.java
@@ -0,0 +1,13 @@
+package com.coupon.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponAapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponAapApplication.class, args);
+	}
+
+}
---
+++ coupon-aap/src/test/java/com/coupon/application/CouponAapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupon.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponAapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
